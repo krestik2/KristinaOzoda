f7e0b3b286693477544e1066132623f06a4b7a5e
krestik2 <107003855+krestik2@users.noreply.github.com>
2022-08-31 16:24:00 -0400

    Create Sunday.java

---
+++ src/Week/Sunday.java
@@ -0,0 +1,15 @@
+
+
+
+
+  public static void main(String[] args) {
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            
+            int a = 10;
+          
+          int b =12;
+            
+        }
+    }
